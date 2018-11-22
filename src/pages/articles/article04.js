5640c6f66256ae7768253c85066d73f70fb9458c
MichaelYLZ <liyang_shifan@163.com>
2018-11-22 19:10:24 +0800

    :12-commit-对第四篇文章更新

--- src/pages/articles/article04.js
+++ src/pages/articles/article04.js
@@ -2,8 +2,19 @@ import React from "react"
 import styles from "./article01.js.module.css"
 import pic27 from "./imgs/27.png"
 import pic28 from "./imgs/28.png"
-
-
+import pic29 from "./imgs/29.png"
+import pic30 from "./imgs/30.png"
+import pic31 from "./imgs/31.png"
+import pic32 from "./imgs/32.png"
+import pic33 from "./imgs/33.png"
+import pic34 from "./imgs/34.png"
+import pic35 from "./imgs/35.png"
+import pic36 from "./imgs/36.png"
+import pic37 from "./imgs/37.png"
+import pic38 from "./imgs/38.png"
+import pic39 from "./imgs/39.png"
+import pic40 from "./imgs/40.png"
+import pic41 from "./imgs/41.png"
 
 export default () => (
 	<article className={styles.container}>
@@ -29,20 +40,26 @@ export default () => (
     <ol>
     
     <a href="#header01">
-    <li>01-webpack 4 无配置文件</li>
+    <li>webpack 4 无配置文件</li>
     </a>
     
     <a href="#header02">
-    <li>02-无配置文件对有配置文件影响</li>
+    <li>无配置状态下默认值以及对对默认值的更改</li>
     </a>
     
-    <a href="#header02">
-    <li>03-webpack 4 配置文件的语法结构</li>
+    <a href="#header03">
+    <li>无配置文件对有配置文件影响</li>
+    </a>
+    
+    <a href="#header04">
+    <li>webpack 4 配置文件的基本结构</li>
     </a>
     
     </ol>
     
     <h2 name='header01'>01-webpack 4 无配置文件</h2>
+    
+    
     
     <div>
     不需要配置文件，是webpack 4的特点之一。
@@ -117,8 +134,173 @@ export default () => (
     </div>
     <br />
     
-    关键词
-    <span className={styles.code}>Gatsby</span>
+    <div>
+    黄色字体部分有一个<span className={styles.code}>warning</span>，这和webpack 4<span className={styles.code}>mode</span>相关。 
+    <br/>
+    mode分两种情况。
+    <br/>
+    01-<span className={styles.code}>development mode</span>。为开发阶段，提供相应的服务，比如，会提供dev server，hot module replacement。
+    <br/>
+    02-<span className={styles.code}>production mode</span>。生成上线版本的代码，最典型的就是minification，去除源码中的空格，备注。当然webpack的优化不止这些。
+    <br/>
+    换以下代码。
+    </div>
+    
+    <br />
+    <div>
+    <img src={pic29} alt="mode相关代码" className={styles.img}/>
+    </div>
+    <br />
+    
+    <div>
+    package.json调整好以后，运行以下代码。
+    </div>
+    <br/>
+    
+    <br />
+    <div className={styles.codeblock}>
+    <pre className={styles.pre}>
+    <code>
+    npm run dev 
+    </code>
+    </pre>
+    </div> 
+    <br />
+    
+    <br />
+    <div>
+    <img src={pic30} alt="dev模式webpack生成的代码" className={styles.img}/>
+    </div>
+    <br />
+    
+    <br />
+    <div>
+    <img src={pic31} alt="命令行信息" className={styles.img}/>
+    </div>
+    <br />
+    
+    <div>
+    运行后，命令行界面不再有<span className={styles.code}>warning</span>提示。
+    <br/>
+    <span className={styles.code}>dist
+    </span>文件夹中，不同于production阶段的代码，是仍然有空格的。
+    <br />
+    运行以下：
+    </div>
+    
+    <br />
+    <div className={styles.codeblock}>
+    <pre className={styles.pre}>
+    <code>
+    npm run build
+    </code>
+    </pre>
+    </div> 
+    <br />
+    
+    <div>
+    <img src={pic32} alt="production code" className={styles.img}/>
+    </div>
+    <br />
+    
+    <h2 name='header02'>02-无配置状态下默认值以及对对默认值的更改</h2>
+    
+    <div>
+    没有配置文件情况下，<span className={styles.code}>entry point</span>的默认值是<span className={styles.code}>./src/index.js</span>。
+    <br />
+    webpack去<span className={styles.code}>src</span>文件夹中寻找<span className={styles.code}>./src/index.js</span>，进行处理，
+    <br/>
+    产出是<span className={styles.code}>./dist/main.js</span>。
+    <br/>
+    在配置文件<span className={styles.code}>webpack.config.js</span>，相应的代码是这样：
+    </div>
+    <br/>
+    
+    <div>
+    <img src={pic33} alt="配置文件相应代码" className={styles.img}/>
+    </div>
+    <br />
+    
+    <div>
+    无配置状态文件下，可以通过修改package.json来实现对默认值的更改：
+    </div>
+    <br/>
+   
+    <div>
+    <img src={pic34} alt="Gatsby大量使用无状态组件" className={styles.img}/>
+    </div>
+    <br />
+    
+    <div>
+    需要你动手实验一下。
+    </div>
+    <br/>
+    
+    <h2 name='header03'>03-无配置文件对有配置文件影响</h2>
+    
+    <div>
+    以上的这些规则都是官网看来了。
+    <br/>
+    无配置文件状态下，是有默认值的。
+    <br/>
+    所以有配置文件时候，<span className={styles.code}>entry</span>和<span className={styles.code}>output</span>，可以不用写。
+    </div>
+    <br/>
+    
+    <h2 name='header04'>04-webpack 4 配置文件的基本结构</h2>
+    
+    <br />
+    <div>
+    插播:对于entry的额外讲解
+    <img src={pic35} alt="entry讲解" className={styles.img}/>
+    </div>
+    <br />
+    
+    <br />
+    <div>
+    插播:自己实验
+    <img src={pic36} alt="entry讲解" className={styles.img}/>
+    </div>
+    <br />
+    
+    <br />
+    <div>
+    插播:额外讲解
+    <img src={pic37} alt="output讲解" className={styles.img}/>
+    </div>
+    <br />
+    
+    <br />
+    <div>
+    <span className={styles.code}>webpack.config.js</span>基本结构01
+    <img src={pic38} alt="配置文件demo" className={styles.img}/>
+    </div>
+    <br />
+    
+    <br />
+    <div>
+    <span className={styles.code}>webpack.config.js</span>基本结构02
+    <img src={pic39} alt="配置文件demo" className={styles.img}/>
+    </div>
+    <br />
+    
+    <br />
+    <div>
+    <span className={styles.code}>webpack.config.js</span>基本结构03
+    <img src={pic40} alt="配置文件demo" className={styles.img}/>
+    </div>
+    <br />
+    
+    <br />
+    <div>
+    <span className={styles.code}>webpack.config.js</span>基本结构04
+    <img src={pic41} alt="配置文件demo" className={styles.img}/>
+    </div>
+    <br />
+    
+    关键词000
+    <span className={styles.code}></span>
+    
     
     图片
     <br />
@@ -132,9 +314,7 @@ export default () => (
     <div className={styles.codeblock}>
     <pre className={styles.pre}>
     <code>
-    mkdir webpack-learning
-    <br />
-    
+    npm run dev 
     </code>
     </pre>
     </div> 
@@ -164,10 +344,6 @@ export default () => (
     </div>
     <br/>
    
-    
-    链接
-    
-    <a href='https://www.netlify.com/blog/2016/02/24/a-step-by-step-guide-gatsby-on-netlify/' target='_blank' rel="noopener noreferrer">具体操作可点击这里</a>
    
     </article>
 )
