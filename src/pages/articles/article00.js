337c96d3644ff5b7ea8717eb3a5b5c3770d75ef8
MichaelYLZ <liyang_shifan@163.com>
2018-12-27 18:37:13 +0800

    33 commit 新增一些链接

--- src/pages/articles/article00.js
+++ src/pages/articles/article00.js
@@ -167,6 +167,36 @@ export default () => (
     </a>
     </li>
     
+    <li>
+    <a href='https://www.codecademy.com/articles/what-is-cors' target='_blank' rel="noopener noreferrer">
+    cors 跨域入门讲解
+    </a>
+    </li>
+    
+    <li>
+    <a href='https://webpack.jakoblind.no/' target='_blank' rel="noopener noreferrer">
+    Webpack 开发神奇/(工具)
+    </a>
+    </li>
+    
+    <li>
+    <a href='http://blog.jakoblind.no/' target='_blank' rel="noopener noreferrer">
+    综合性资源01-挪威程序员-React-webpack-redux相关
+    </a>
+    </li>
+    
+    <li>
+    <a href='https://www.techiediaries.com/' target='_blank' rel="noopener noreferrer">
+    综合性资源02-React-Vue-Angular-Django
+    </a>
+    </li>
+    
+    <li>
+    <a href='https://www.sohamkamani.com/blog' target='_blank' rel="noopener noreferrer">
+    综合性资源03-Soham
+    </a>
+    </li>
+    
     
     </ol>
     
@@ -225,56 +255,6 @@ export default () => (
     </a>
     </li>
     
-    </ol>
-  
-    
-    
-    
-    
-    
-    
-    
-    <a href='' target='_blank' rel="noopener noreferrer">具体操作可点击这里</a><br/>
-    
-    关键词
-    <span className={styles.code}>Gatsby</span>
-    
-    图片
-    <br />
-    <div>
-    <img src={pic01} alt="Gatsby大量使用无状态组件" className={styles.img}/>
-    </div>
-    <br />
-    
-    可复制代码块01
-    
-    <div className={styles.codeblock}>
-    <pre className={styles.pre}>
-    <code data-lang="javascript">
-    import ApolloClient from 'apollo-boost';
-    </code>
-    </pre>
-    </div> 
-    
-    
-	<div>
-    正文
-    </div>
-    <br/>
-    
-    文章小节链接--需修改
-    <ol>
-    <a href="#header01">
-    <li>不只react</li>
-    </a>
-    <a href="#header02">
-    <li>Gatsby使用体验</li>
-    </a>
-    </ol>
-    
-    链接
-    
-    <a href='baidu.com' target='_blank' rel="noopener noreferrer">具体操作可点击这里</a>
-   
+    </ol> 
     </article>
 )
