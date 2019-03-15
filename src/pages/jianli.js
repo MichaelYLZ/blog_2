c2d1dc8eb8b75caa27d44351b1deaa75de68f9ff
MichaelYLZ <liyang_shifan@163.com>
2019-03-15 11:45:58 +0800

    54 commit 调整

--- src/pages/jianli.js
+++ src/pages/jianli.js
@@ -18,8 +18,6 @@ export default () => (
     随时到岗<br />
     177-1289-6753 <br />liyang_shifan@163.com
     <br />
-    <a href='https://liyang2019.netlify.com/' className="link" target='_blank'>React个人博客</a><br />
-    <code>接受一个月的无薪实习</code><br />
     </p>
     
     <h2 className="head extra">
@@ -27,44 +25,95 @@ export default () => (
     </h2>
                                
     <p className="data">
-    前端
+    英语技术支持
     </p>
     
     <h2 className="head extra">
-    技能介绍
+    西餐厅POS技能/知识介绍
     </h2>
                                
     <p className="data">
-    <code>热爱</code>编程技术<br /><br />
-    
-    <code>无障碍阅读</code>英文技术博客/文档<br /><br />
-        
-    理解<code>代码可读性</code>重要性<br /><br />
-    
-    <strong>前端</strong><br /><br />
-    
-    扎实的<code>原生JavaScript</code>基本功，熟练使用 <code>ES6</code><br /><br />
-    
-    熟悉原生<code>Ajax</code>，熟练使用<code>Axios</code><br /><br />
-    
-    熟练使用<code>React</code><br /><br />
-    
-    
-    掌握<code>Webpack 4</code>的使用，会打包构建基于react框架的web前端应用，熟悉常见loader和plugin<br /><br />
-    
-    <strong>后端</strong><br /><br />
-    
-    
-    熟悉<code>Node</code>和<code>Express</code>，可以用<code>Express</code>框架搭配<code>MongoDB</code>实现<code>CRUD</code>功能<br /><br />
-    
-    因为可以实现基础的后端功能，所以对<code>前后端交互</code>有比较好的理解<br /><br />
-    
-    熟悉<code>SQL</code>基础语句，熟悉<code>MySQL</code>基础命令行操作
+    熟练-高效使用餐厅POS机功能；<br />
+    了解餐厅经营中，可能出现的情况，以及这类情况对POS机的影响，<br />
+    比如新菜上市，POS机肯定需要更新；<br />
+    餐厅座位调整，POS也需要更新；<br />
+    有些高级功能，<br />
+    如查看营业额，结账，<br />
+    普通服务员是没有权限操作的。<br />
+    技术支持岗位中，可能需要了解此类功能，我也可以很好很快的理解。
     </p>
+    
+    <h2 className="head extra">
+    编程技能介绍
+    </h2>
+                               
+    <p className="data">
+    目前主要编程技能是在前端，<br />
+    我对<code>JavaScript</code>有比较好的掌握。<br />
+    
+    可以用<code>Node</code>实现一些基础的服务端功能。<br />
+    
+    熟练掌握<code>SQL</code>基本语句，熟练进行<code>MySQL</code>数据库常规操作。
+    <br /><br />
+    
+    技术支持岗位需要对操作系统和网络知识有很好的掌握，<br />
+    因为我对产品本身有很好的理解，<br />
+    我自己进行一些有针对性的学习，请教相关开发人员，<br />
+    我相信自己可以比较快的适应。
+    </p>
+    
+    <h2 className="head extra">
+    英语技能介绍
+    </h2>
+                               
+    <p className="data">
+    我是科班学英语的。<br />
+    我初中毕业以后，<br />
+    念了我们高邮当地的师范英语专业。<br />
+    我师范二年级，类似高二，通过了大学英语四级；<br />
+    师范三年级，类似高三，通过了大学英语六级；<br /><br />
+    
+    学校课程很细，<br />
+    有外教，我也喜欢和外国人交朋友，<br />
+    理论联系实际，<br />
+    进步的比较快。<br /><br />
+    
+    最近学习编程，<br />
+    我也是全部使用原版美国教材，<br />
+    基本天天都会阅读英语技术书籍，博客。
+    </p>
+    
+    
+    
  
     <h2 className="head extra">
     工作经历
     </h2>
+    
+    
+    
+    <div className="flexParent">
+    <div className="flex1 time">
+    2017年3月-2018年1月
+    </div>
+    
+    <div className="flex2">
+    南京新元素西餐厅<br />
+    职位：兼职服务员<br />
+    待遇：17元/小时<br /><br />
+    按照餐厅标准，进行开档，客人接待，给客人点单，POS机下单，上菜，结账，餐厅收档。<br /><br />(因为我是兼职，大部分时间只上一个班次。)<br /><br />
+    学习菜单，学习使用POS机，来对接后厨。<br /><br />
+    参加新菜，酒类培训。<br /><br />
+    非高峰期餐点，按照值班经理安排，打扫卫生。<br /><br />
+    
+    <strong>工作业绩</strong><br />
+    了解西餐，能独立的接待/处理特殊要求客人，比如素食主义者客人，不同类型的过敏客人；<br /><br />
+    假如有一大桌外国客人，他们可能会分开买单，一般值班经理会安排我去接待；<br /><br />
+    亲和的态度，良好的沟通技能，为餐厅办了不少会员卡；<br /><br />
+    拿到客人小费；<br /><br />
+    和一些外国客人，私底下成为好朋友，有些现在还联系；
+    </div>
+    </div>
                                
     <div className="flexParent">
     <div className="flex1 time">
@@ -74,8 +123,13 @@ export default () => (
     <div className="flex2">
     菲律宾福乐威房地产有限公司<br />
     职位：英语翻译<br />
-    待遇：4k-5k<br />
-    这是一个菲律宾房地产项目。我负责翻译老板和菲律宾同事的来往邮件。各项许可证于2016年11份上半旬下来。2017年1月4号至2017年1月14号陪同老板赴菲出差。工作后期需要长期驻菲律宾工作，个人对菲律宾气候不是很适应，后来提出了离职。
+    待遇：4k-5k<br /><br />
+    这是一个菲律宾房地产项目。<br />
+    我负责翻译老板和菲律宾同事的来往邮件。<br />
+    各项许可证于2016年11份上半旬下来。<br />
+    2017年1月4号至2017年1月14号陪同老板赴菲出差。<br />
+    工作后期需要长期驻菲律宾工作，<br />
+    个人对菲律宾气候不是很适应，后来提出了离职。
     </div>
     </div>
     
@@ -87,8 +141,15 @@ export default () => (
     <div className="flex2">
     华辉国际运输服务有限公司江苏分公司<br />
     职位：英语翻译<br />
-    待遇：4k-5k<br />
-    公司做私人物品的国际运输业务，其中以海外出口为主，服务客户主要是省内的欧美外企。内勤发来工作指令后，我和打包工人，司机一齐准时去客人家。我主要负责和客人的沟通，做出口物品明细清单。这个清单很重要，国内国外海关都会看。后来因江苏分公司业绩不理想，被公司裁员。
+    待遇：4k-5k<br /><br />
+    公司做私人物品的国际运输业务，<br />
+    其中以海外出口为主，<br />
+    德国客户比多。
+
+    <br /><br />
+    我主要负责和客人的沟通，讲解我们工作流程，一些海关要求，做出口物品明细清单。<br />
+    这个清单很重要，国内国外海关都会看。<br />
+    后来因江苏分公司业绩不理想，被公司裁员。
     </div>
     </div>
     
